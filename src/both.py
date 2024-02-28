337b575b4d029e18ccb33b5c1d380ace6683d7dd
misterrey <maksropcan@gmail.com>
2024-02-28 23:07:39 +0200

    Task 5 done

--- src/both.py
+++ src/both.py
@@ -1,11 +1,9 @@
-def calculate_fuel(distance):
-    if isinstance(distance, (int, float)) and distance > 0:
-        fuel_required = distance * 10
-        return max(fuel_required, 100)  # Повертає більше значення з fuel_required та 100
-    else:
-        return 100  # Повертає 100, якщо відстань не відповідає умовам
+def both(number1, number2):
+    return (number1 <= 0 and number2 <= 0) or (number1 >= 0 and number2 >= 0) or (number1 == 0 and number2 == 0)
 
 
-print(calculate_fuel(20))  # Поверне 200
-print(calculate_fuel(8))   # Поверне 100 (тому що мінімум 100 літрів)
-print(calculate_fuel(-5))  # Поверне 100 (тому що відстань менша або дорівнює 0)
+print(both(-1, -5))  # Поверне True
+print(both(3, 7))     # Поверне True
+print(both(0, -8))    # Поверне False
+print(both(-2, 4))    # Поверне False
+print(both(0, 0))     # Поверне True
