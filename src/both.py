cbc61e91a1b7ed8113bb061c7afe9ee2b0f5cc29
misterrey <maksropcan@gmail.com>
2024-02-28 22:55:04 +0200

    Task 5 done

--- src/both.py
+++ src/both.py
@@ -1,9 +1,6 @@
 def both(number1, number2):
     return (number1 <= 0 and number2 <= 0) or (number1 >= 0 and number2 >= 0)
 
-
-
-print(both(-1, -5))  # Поверне True
 print(both(3, 7))  # Поверне True
 print(both(0, -8))  # Поверне True
 print(both(-2, 4))  # Поверне False
