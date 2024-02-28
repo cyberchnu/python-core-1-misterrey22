55bead4a56b3f10534ec960b962095ea2453a9ab
misterrey <maksropcan@gmail.com>
2024-02-28 22:57:32 +0200

    Task 5 done

--- src/both.py
+++ src/both.py
@@ -1,3 +1,14 @@
-def both(number1, number2):
-    return (number1 <= 0 and number2 <= 0) or (number1 >= 0 and number2 >= 0)
+def calculate_fuel(distance):
+    if distance <= 0:
+        return 100  # Повертаємо 100, якщо відстань менша або дорівнює нулю
+    else:
+        fuel_needed = distance * 10
+        if fuel_needed < 100:
+            return 100  # Повертаємо 100, якщо розраховане значення менше за 100
+        else:
+            return fuel_needed
 
+
+distance_to_travel = 20  # Приклад відстані в кілометрах
+required_fuel = calculate_fuel(distance_to_travel)
+print("Об'єм палива для проїзду", distance_to_travel, "км:", required_fuel, "л")
